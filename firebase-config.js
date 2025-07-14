8baf071bf751799a0faa79fd5810df5440791bc8
Rahul Thakur <164646992+Rahulthakur9124@users.noreply.github.com>
2025-07-14 16:14:08 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfvQxsNKceB8m-kzSCzmyx9XoE4rtSzis",
+  authDomain: "stressprediction-21e62.firebaseapp.com",
+  projectId: "stressprediction-21e62",
+  storageBucket: "stressprediction-21e62.firebasestorage.app",
+  messagingSenderId: "850598563024",
+  appId: "1:850598563024:web:1874a041e642af242062d5",
+  measurementId: "G-G0D55BQ3RW"
+};
+
+firebase.initializeApp(firebaseConfig);
